c3393018ee79592a29ff04c368b61453e9cf86e4
mihaiCatalin15 <atudosieacatalin@gmail.com>
2020-07-25 22:46:39 +0300

    Updates

---
+++ precache-manifest.4baf02352169ceb8021676c6c8fd253f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af144bbf17dba4e2502e8681b6408ab",
+    "url": "/mishuleu15/Shopping-Cart/index.html"
+  },
+  {
+    "revision": "bbecdc7e037061c42b30",
+    "url": "/mishuleu15/Shopping-Cart/static/css/main.f8fb891a.chunk.css"
+  },
+  {
+    "revision": "6c7f2b70049208d0a11f",
+    "url": "/mishuleu15/Shopping-Cart/static/js/2.5b012ebe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mishuleu15/Shopping-Cart/static/js/2.5b012ebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbecdc7e037061c42b30",
+    "url": "/mishuleu15/Shopping-Cart/static/js/main.dae72311.chunk.js"
+  },
+  {
+    "revision": "c944bf8265f3af5493d7",
+    "url": "/mishuleu15/Shopping-Cart/static/js/runtime-main.09f463c6.js"
+  }
+]);
